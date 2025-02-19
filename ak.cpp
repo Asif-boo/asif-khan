49e45f94b1f26aa45f0b833d253bd717e0c3b681
Asif-boo <eng.asifkhan.uom@gmail.com>
2025-02-19 10:31:15 -0800

    Add files via upload
    size of a program.

---
+++ ak.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+float x=500;
+float & y=x;
+cout<<x<<endl;
+cout<<y<<endl;
+float e=34.8F;
+long double r=35.9L;
+
+//my first program dated 26/11/2024
+cout<<"the value of e"  << e  <<"\n the value of r" << r;
+          cout <<"\nthe size of 34.8F " <<sizeof (34.8);
+          
+          
+          
+           cout <<"\n the size of 34.8 " <<sizeof (34.8);
+         cout<<"\nthe size of 35.9L " <<sizeof (35.9);
+         
+         
+         
+cout<<"\n the size of 35.9 " <<sizeof (35.9);
+cout<<"\n the multiplication of two numbers  "  << e*r;
+cout<<"\n the division of two numbers "<< r/e;
+             //asif khan the AI MEN 7
+cout<<"\n thi is AI men ASIF KHAN 7";
+cout<<"\n at the department of AI in the university oof malakand";
+ 
+  x=500;
+   y=x;
+ cout << "\n the value of x is " <<x <<  " and the value of y   is  " <<y;
+                cout<<"\n multiplication of two numbers "  << x*y;
+               cout<<"\n the division of two numbers "<<x/y;
+               
+               
+ // i am very happy today becuse this my longest program i run today
+ cout<<"\n the length is "<<y<<"\n and the width is "<<x;
+ 
+ 
+ cout<<"\n the area of the wall "<<x*y;
+  cout<<"\n"<< 4+4;                 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+return 0;
+}
+
+
+
